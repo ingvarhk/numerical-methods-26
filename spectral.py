4661dc62e76e4950490f109e1c31701b1f804117
T0kers <tokerskovhuus@gmail.com>
2026-08-17 10:39:10 +0200

    lavet spectral funktion

--- spectral.py
+++ spectral.py
@@ -1,39 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+def spectral(dx, dy, l, x0, P0, sigma, epsilon, eta):
+    x = np.arange(0, l, dx)
+    y = np.arange(0, l, dy)
+
+    Nx = len(x)
+    Ny = len(y)
+
+    X, Y = np.meshgrid(x, y)
+
+    P = P0 * np.exp(-(X - x0)**2 / (2 * sigma**2))
+
+    kx = 2 * np.pi * np.fft.fftfreq(Nx, dx)
+    ky = 2 * np.pi * np.fft.fftfreq(Ny, dy)
+
+    Kx, Ky = np.meshgrid(kx, ky)
+    K_squared = Kx**2 + Ky**2
+
+
+    vx_tilde = - 1j * Kx * np.fft.fft2(P) / (epsilon + K_squared * eta)
+
+    vy_tilde = - 1j * Ky * np.fft.fft2(P) / (epsilon + K_squared * eta)
+
+    vx = np.fft.ifft2(vx_tilde).real
+    vy = np.fft.ifft2(vy_tilde).real
+
+    return x, y, X, Y, P, vx, vy
+
+
 dx = 0.5
 dy = 0.5
 l = 40
-
-x = np.arange(0, l, dx)
-y = np.arange(0, l, dy)
-
-Nx = len(x)
-Ny = len(y)
-
-X, Y = np.meshgrid(x, y)
 
 epsilon = 12.5
 eta = 40
 
 x0 = l / 2 # 20
+
 sigma = 1.2
 P0 = 10
-P = P0 * np.exp(-(X - x0)**2 / (2 * sigma**2))
 
-kx = 2 * np.pi * np.fft.fftfreq(Nx, dx)
-ky = 2 * np.pi * np.fft.fftfreq(Ny, dy)
-
-Kx, Ky = np.meshgrid(kx, ky)
-K_squared = Kx**2 + Ky**2
-
-
-vx_tilde = - 1j * Kx * np.fft.fft2(P) / (epsilon + K_squared * eta)
-
-vy_tilde = - 1j * Ky * np.fft.fft2(P) / (epsilon + K_squared * eta)
-
-vx = np.fft.ifft2(vx_tilde).real
-vy = np.fft.ifft2(vy_tilde).real
+x, y, X, Y, P, vx, vy = spectral(dx, dy, l, x0, P0, sigma, epsilon, eta)
 
 fig, ax = plt.subplots(ncols=2)
 imx = ax[0].imshow(vx, origin="lower")
