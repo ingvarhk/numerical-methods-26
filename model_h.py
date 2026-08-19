bd1fa6277644d68e2c2a1488ec42bb0ae500d39b
ingvarhk <ingvar@hahnkristensen.dk>
2026-08-19 11:05:17 +0200

    små ændringer

--- model_h.py
+++ model_h.py
@@ -11,7 +11,7 @@ x = np.arange(0, c.lx, c.dx)
 y = np.arange(0, c.ly, c.dy)
 X, Y = np.meshgrid(x, y)
 
-#phi = np.random.random((len(x), len(y)))*1.5-1
+phi = np.random.random((len(x), len(y)))*1.5-1
 
 R = np.sqrt((X - c.x0)**2 + (Y - c.y0)**2)
 phi = (R < c.R0)*1.0
@@ -19,20 +19,25 @@ phi = (R < c.R0)*1.0
 phi[phi > 0.5] = 1
 phi[phi < 0.5] = -np.sqrt(c.b/c.a)
 
+velocity_function = constant_velocity
+
 # Simulation
-T, PHI, MEAN_PHI = cahn_hilliard(phi, 100, 200, stokes_flow)
+T, PHI, MEAN_PHI = cahn_hilliard(phi, 200, 200, velocity_function)
+
+plt.plot(T, MEAN_PHI, ".")
+plt.show()
 
 # Animation
 fig, ax = plt.subplots(figsize=(10, 7))
 anim = get_animation(T, PHI, fig, ax)
 
-# Plot hastighed
-vx, vy = stokes_flow(phi)
-ax.quiver(*rebin_velocity_field(X, Y, vx, vy), scale=5)
+# Plot hastighed ved start
+vx, vy = velocity_function(phi)
+ax.quiver(*rebin_velocity_field(X, Y, vx, vy), scale=np.max(vx), scale_units="xy")
 
 # Gammelt hastighedsplot
 #ax2 = add_secondary_axis(ax)
 #ax2.plot(x, vx[0], "--", color="red", lw=2)
 
 plt.show()
-#anim.save("animation.gif", writer="pillow", fps=20)
+anim.save("animation.gif", writer="pillow", fps=20)
