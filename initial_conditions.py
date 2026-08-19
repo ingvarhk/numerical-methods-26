a3ebc79eb9a80afd7f9182bf840ad57087f68229
ingvarhk <ingvar@hahnkristensen.dk>
2026-08-19 11:32:40 +0200

    initial conditions klasse

---
+++ initial_conditions.py
@@ -0,0 +1,24 @@
+import constants as c
+import numpy as np
+
+class InitialConditions:
+    def __init__(self):
+        self.x = np.arange(0, c.lx, c.dx)
+        self.y = np.arange(0, c.ly, c.dy)
+        self.X, self.Y = np.meshgrid(self.x, self.y)
+
+        phi_equilibrium = np.sqrt(c.b/c.a)
+        self.phi = np.zeros((len(self.x), len(self.y))) - phi_equilibrium # Stable background
+
+    def add_random_bubbles(self):
+        self.phi += np.random.random(np.shape(self.phi))*1.5-1
+
+    def add_pyrenoid(self, value: float = 1.0):
+        R = np.sqrt((self.X - c.x0)**2 + (self.Y - c.y0)**2)
+        self.phi[R < c.R0] = value
+
+    def add_stardust(self, scale: float = 0.1):
+        self.phi += (np.random.random(np.shape(self.phi))-0.5)*scale # Star dust
+
+    def get_phi(self):
+        return self.phi
--- model_h.py
+++ model_h.py
@@ -5,39 +5,33 @@ from cahn_hilliard import cahn_hilliard
 from velocity_fields import stokes_flow, constant_velocity, zero_velocity
 
 from ui import get_animation, add_secondary_axis, rebin_velocity_field
+from initial_conditions import InitialConditions
+
 import constants as c
 
-x = np.arange(0, c.lx, c.dx)
-y = np.arange(0, c.ly, c.dy)
-X, Y = np.meshgrid(x, y)
+# Vælge startbetingelser
+IC = InitialConditions()
+IC.add_pyrenoid()
 
-phi = np.random.random((len(x), len(y)))*1.5-1
-
-R = np.sqrt((X - c.x0)**2 + (Y - c.y0)**2)
-phi = (R < c.R0)*1.0
-
-phi[phi > 0.5] = 1
-phi[phi < 0.5] = -np.sqrt(c.b/c.a)
-
+# Vælg velocity-funktion
 velocity_function = constant_velocity
 
-# Simulation
-vel = velocity_function(phi)
-T, PHI, MEAN_PHI = cahn_hilliard(phi, 200, 200, lambda _: vel)
+# Kør simulation
+vel = velocity_function(IC.phi)
+T, PHI, MEAN_PHI = cahn_hilliard(IC.phi, 200, 200, lambda _: vel)
 
-print(PHI.shape)
 
-# Animation
+# Lav animation
 fig, ax = plt.subplots(figsize=(10, 7))
 anim = get_animation(T, PHI, fig, ax)
 
-# Plot hastighed ved start
-vx, vy = velocity_function(phi)
-ax.quiver(*rebin_velocity_field(X, Y, vx, vy), scale=np.max(vx), scale_units="xy")
+# Tegn initial hastighedsfelt
+vx, vy = velocity_function(IC.phi)
+ax.quiver(*rebin_velocity_field(IC.X, IC.Y, vx, vy), scale=np.max(vx), scale_units="xy")
 
 # Gammelt hastighedsplot
 #ax2 = add_secondary_axis(ax)
 #ax2.plot(x, vx[0], "--", color="red", lw=2)
 
 plt.show()
-anim.save("animation.gif", writer="pillow", fps=20)
+#anim.save("animation.gif", writer="pillow", fps=20)
